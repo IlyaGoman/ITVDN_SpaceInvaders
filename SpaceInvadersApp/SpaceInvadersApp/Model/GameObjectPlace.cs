408850f456066cd5c24ea907499377991ff875d3
goman_iv <realtt@mail.ru>
2020-10-22 14:02:52 +0300

    added main classes to model

---
+++ SpaceInvadersApp/SpaceInvadersApp/Model/GameObjectPlace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvadersApp.Model
+{
+    class GameObjectPlace
+    {
+        public int XCoordinate { get; set; }
+
+        public int YCoordinate { get; set; }
+    }
+}
---
+++ SpaceInvadersApp/SpaceInvadersApp/Model/GameObjectType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvadersApp.Model
+{
+    enum GameObjectType
+    {
+        AllianShip = 1,
+        PlayerShip = 2,
+        PlayerShipMissile = 3,
+        GroudObject = 4
+    }
+}
